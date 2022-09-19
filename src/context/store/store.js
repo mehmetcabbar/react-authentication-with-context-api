912fbb02d32ffc23e65dbde79e82aa80892068c4
mehmetcabbar <mhmtcbbr@gmail.com>
2022-09-19 10:52:11 +0300

    Last change for state

--- src/context/store/store.js
+++ src/context/store/store.js
@@ -1,15 +1,13 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
 export const user = {
     email: 'example@example.com',
-    setEmail: '',
     password: '12345678',
-    setPassword: '',
     isLoggedIn: false,
     setIsLoggedIn: '',
     myEmail: 'mhmtcbbr@gmail.com'
 }
 
-const Context = React.createContext(user);
+const Context = createContext(user);
 
 export default Context;
